60e9f5897c82597d8bb58c7ed7919d503c4f4ea8
duongbui123 <buiducduong2342000@gmail.com>
2021-11-13 21:08:56 +0700

    electron reload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const isDev = !app.isPackaged;
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -17,7 +18,13 @@ function createWindow() {
   win.loadFile('index.html');
 
   // Open the Devtools.
-  win.webContents.openDevTools();
+  isDev && win.webContents.openDevTools();
+}
+
+if (isDev) {
+  require('electron-reload')(__dirname, {
+    electron: path.join(__dirname, 'node_modules', '.bin', 'electron'),
+  });
 }
 
 app.whenReady().then(() => {
