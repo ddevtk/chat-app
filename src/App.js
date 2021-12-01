f1db32e01744331744be904dfc53734c1a997702
duongbui123 <buiducduong2342000@gmail.com>
2021-12-01 10:42:03 +0700

    setup routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { ipcRenderer } from 'electron';
 import Home from './views/Home';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 const App = () => {
   const sendNotification = () => {
     window.sayHello('Hello from App.js');
   };
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <Navbar />
+      <div className='content-wrapper'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/settings' element={<h1>Settings</h1>} />
+          <Route path='/register' element={<h1>Register</h1>} />
+          <Route path='/login' element={<h1>Login</h1>} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import AvailableChat from '../components/AvailableChats';
 import JoinedChat from '../components/JoinedChats';
-import Navbar from '../components/Navbar';
 
 const Home = () => {
   return (
-    <div className='content-wrapper'>
-      <Navbar />
-      <div className='row no-gutters fh'>
-        <div className='col-3 fh'>
-          <JoinedChat />
-        </div>
-        <div className='col-9 fh'>
-          <AvailableChat />
-        </div>
+    <div className='row no-gutters fh'>
+      <div className='col-3 fh'>
+        <JoinedChat />
+      </div>
+      <div className='col-9 fh'>
+        <AvailableChat />
       </div>
     </div>
   );
