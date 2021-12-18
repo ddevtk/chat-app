13ddcab73f67570b356019f6ee60f4e9139e0333
duongbui123 <buiducduong2342000@gmail.com>
2021-12-18 16:13:27 +0700

    create chat form

--- src/views/ChatCreate.js
+++ src/views/ChatCreate.js
@@ -1,10 +1,78 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+
 import Base from '../layouts/Base';
 
 const ChatCreate = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <Base canGoBack>
-      <h1>Chat create</h1>
+      <div className='centered-view'>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className='centered-container-form'
+        >
+          <div className='header'>Create chat now 👋👋👋</div>
+          <div className='subheader'>Chat with your friend </div>
+          <div className='form-container'>
+            <div className='form-group'>
+              <label htmlFor='name'>Name</label>
+              <input
+                type='text'
+                className='form-control'
+                id='name'
+                {...register('name', { required: true })}
+              />
+              {/* {errors.email && (
+                <small style={{ color: '#bf1650' }}>
+                  This field is required
+                </small>
+              )} */}
+            </div>
+            <div className='form-group'>
+              <label htmlFor='description'>Description</label>
+              <textarea
+                type='text'
+                className='form-control'
+                id='description'
+                {...register('description', {
+                  required: true,
+                })}
+              />
+              {/* {errors.password && errors.password.type === 'required' && (
+                <small style={{ color: '#bf1650' }}>
+                  This field is required!
+                </small>
+              )} */}
+            </div>
+            <div className='form-group'>
+              <label htmlFor='image'>Image</label>
+              <input
+                type='text'
+                className='form-control'
+                id='image'
+                {...register('image', { required: true })}
+              />
+              {/* {errors.email && (
+                <small style={{ color: '#bf1650' }}>
+                  This field is required
+                </small>
+              )} */}
+            </div>
+            {false && <div className='alert alert-danger small'></div>}
+            <button type='submit' className='btn btn-outline-primary'>
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
     </Base>
   );
 };
